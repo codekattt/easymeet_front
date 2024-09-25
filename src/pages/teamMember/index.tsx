a1271f1b45355409bb22d6ff9eec8154da62ccc1
codekattt <widkid00@naver.com>
2024-09-25 17:20:03 +0900

    fix:팀멤버 라우터 이슈 해결

--- src/pages/teamMember/index.tsx
+++ src/pages/teamMember/index.tsx
@@ -4,9 +4,13 @@ import Join from '../../components/units/teamMember/Join/join.index';
 import SelectTime from '../../components/units/teamMember/SelectTime/selectTime.index';
 import Summary from '../../components/units/teamMember/Summary/summary.index';
 
-export default function TeamLeaderPage(): JSX.Element {
+export default function TeamMemberPage(): JSX.Element {
   const router = useRouter();
   const { step } = router.query;
+
+  if (!router.isReady || !step) {
+    return <div>Loading...</div>;
+  }
 
   const renderStepComponent = () => {
     switch (step) {
@@ -17,14 +21,8 @@ export default function TeamLeaderPage(): JSX.Element {
       case 'summary':
         return <Summary />;
       default:
-        return null;
+        return <div>잘못된 경로입니다.</div>;
     }
-  };
-
-  const goToStep = (nextStep: string) => {
-    router.push(`teammember/?step=${nextStep}`, undefined, {
-      shallow: true,
-    });
   };
 
   return (
@@ -32,10 +30,10 @@ export default function TeamLeaderPage(): JSX.Element {
       <AnimatePresence mode="wait">
         <motion.div
           key={step as string}
-          initial={{ opacity: 0, x: 0 }} // 초기 상태 수정: x 축 이동 없음
-          animate={{ opacity: 1, x: 0 }} // 애니메이션 효과: x 축 이동 없음
-          exit={{ opacity: 0, x: 0 }} // 페이지 나갈 때 효과: x 축 이동 없음
-          transition={{ duration: 0.1 }} // 전환 시간
+          initial={{ opacity: 0, x: 0 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 0 }}
+          transition={{ duration: 0.1 }}
         >
           {renderStepComponent()}
         </motion.div>
